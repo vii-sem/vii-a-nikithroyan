b241858c70a2b82845477383ae2119bcef376f48
ENG18CS0054 - Evans Nikith Royan <83712679+nikithroyan@users.noreply.github.com>
2021-10-20 19:08:00 +0530

    Delete WEEK10 directory

--- WEEK10/databaseservice.js
+++
@@ -1,8 +0,0 @@
-class DatabaseService {
-    save(email, price, timestamp) {
-        console.log(`Running query: INSERT INTO orders VALUES (email, price, created) VALUES (${email}, ${price}, ${timestamp})`);
-    }
- }
-  
- module.exports = DatabaseService
- 
